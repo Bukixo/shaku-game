cf21c9b68ea61db5177b2b20181cf04d0c877be5
Bukixo <bukithompson@gmail.com>
2019-02-28 22:04:49 +0000

    looping over arrays to add a colour

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 console.log("app works")
+const colourId = ['one','two','three'];
+const level = 3
 
-// const array = ['one','two','three'];
 
-  function myFunction(arr) {
-    const arr = ['one','two','three'];
-    for (i = 0; i < arr.length; i++) { 
-        console.log(arr[i])
-        document.getElementById(arr[i]).style.background = "pink";
-      }
-   
+
+function loopingColours(array) {
+  const newArray = [];
+  arrayLength = array.length
+  for(var i = 0; i < level; i++){
+    const randomID = Math.floor(Math.random()*array.length);
+    (function(i){
+        setTimeout(function(){
+        document.getElementById(array[randomID]).className = 'newclass';; 
+        }, 2000 * (i)); /// delays the timer
+        newArray.push(randomID);
+        console.log('newArray ', newArray);
+    })(i); //the i picks up the from the for loop and initalliy sety it to a 
   }
+}
 
-  myFunction(array)
+//explain what iffy is anmd how it contributes to setting a delay on each loop.
+
+function gamePlay() {
+  loopingColours(colourId)
+}
 
 
-  
 
-//   setTimeout(function(){
-//     document.getElementById(x).style.background = "pink";
-// }, 
-// 3000);
-// });
